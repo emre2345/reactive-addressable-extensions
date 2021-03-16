f04d8c9645b94cd83d893af14aa5af9d657ec45f
Sitki Emre SOLAK <s.emresolak@dreamharvesters.com>
2021-03-16 12:56:10 +0300

    namespace

--- AssetReferenceExtensions.cs
+++ AssetReferenceExtensions.cs
@@ -4,7 +4,7 @@ using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using Object = UnityEngine.Object;
 
-namespace GameAssets.Scripts
+namespace ReactiveAddressableExtensions
 {
     public static class AssetReferenceExtensions
     {
